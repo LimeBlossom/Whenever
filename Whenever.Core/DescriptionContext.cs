9212946fb52911894fc234f38f562f96af91e2a0
Dan Miller <dsmiller95@gmail.com>
2024-02-13 23:00:19 -0600

    add more to the simple description context

--- Whenever.Core/DescriptionContext.cs
+++ Whenever.Core/DescriptionContext.cs
@@ -1,4 +1,6 @@
-﻿public interface IDescribeCombatants
+﻿using System.Collections.Generic;
+
+public interface IDescribeCombatants
 {
     public string NameOf(CombatantId id);
 }
@@ -20,10 +22,22 @@ public class SimpleDescriptionContext : IDescriptionContext
         this.TargetName = targetName;
     }
 
+    public SimpleDescriptionContext(string initiatorName, string targetName, Dictionary<CombatantId, string> names)
+    {
+        this.InitiatorName = initiatorName;
+        this.TargetName = targetName;
+        this.names = names;
+    }
+    
+    private Dictionary<CombatantId, string> names = new();
     public string InitiatorName { get; set; }
     public string TargetName { get; set;  }
     public string NameOf(CombatantId id)
     {
+        if(names.TryGetValue(id, out var name))
+        {
+            return name;
+        }
         return "Combatant #" + id.ToString();
     }
 }
