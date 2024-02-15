a02cfd49fca69e20cd6e613dc274840ce3b0da0f
Dan Miller <dsmiller95@gmail.com>
2024-02-15 16:35:49 -0600

    add an option to construt a combatant id from a string hash

--- Whenever.Core/CombatantId.cs
+++ Whenever.Core/CombatantId.cs
@@ -1,13 +1,20 @@
 ﻿public record CombatantId
 {
     private readonly int id;
+    // allows usage of the id field to store hashes safely, alongside the default sequencing constructor
+    private readonly bool isHashed;
     
-    public static readonly CombatantId DEFAULT = new CombatantId(1);
+    public static readonly CombatantId DEFAULT = new CombatantId(1, false);
     public static readonly CombatantId INVALID = default;
     
     public static CombatantId Next(CombatantId id)
     {
-        return new CombatantId(id.id + 1);
+        return new CombatantId(id.id + 1, false);
+    }
+
+    public static CombatantId Hashed(string name)
+    {
+        return new CombatantId(name.GetHashCode(), true);
     }
 
     public CombatantId Next()
@@ -15,10 +22,12 @@
         return CombatantId.Next(this);
     }
     
-    private CombatantId(int id)
+    private CombatantId(int id, bool isHashed)
     {
         this.id = id;
+        this.isHashed = isHashed;
     }
+    
         
     public override string ToString()
     {
