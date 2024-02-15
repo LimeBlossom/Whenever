2dd05ccc4e9f5acac48abbe1d961b66a645bff0e
Dan Miller <dsmiller95@gmail.com>
2024-02-14 23:02:45 -0600

    add whenever filter that is never met

---
+++ Whenever.Core/WheneverFilter/NeverWheneverFilter.cs
@@ -0,0 +1,14 @@
+﻿public class NeverWheneverFilter<TInspectWorld, TCommandWorld>: IWheneverFilter<TInspectWorld, TCommandWorld>
+    where TInspectWorld : IInspectWorld
+    where TCommandWorld : ICommandWorld
+{
+    public bool TriggersOn(InitiatedCommand<TCommandWorld> initiatedCommand, TInspectWorld world)
+    {
+        return false;
+    }
+
+    public string Describe(IDescriptionContext context)
+    {
+        return "never";
+    }
+}
