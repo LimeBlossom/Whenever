e9039d4e70314ff57f09306888b299f0cf9d902a
Dan Miller <dsmiller95@gmail.com>
2024-02-15 15:28:12 -0600

    implement the specific whenever filter factories

---
+++ Whenever.Core/WheneverFilter/InitiatorIsExactly.cs
@@ -0,0 +1,26 @@
+﻿public class InitiatorIsExactly<TInspectWorld, TCommandWorld>: IWheneverFilter<TInspectWorld, TCommandWorld>
+    where TInspectWorld : IInspectWorld
+    where TCommandWorld : ICommandWorld
+{
+    private readonly CombatantId id;
+
+    public InitiatorIsExactly(CombatantId id)
+    {
+        this.id = id;
+    }
+    
+    public bool TriggersOn(InitiatedCommand<TCommandWorld> initiatedCommand, TInspectWorld world)
+    {
+        if (!initiatedCommand.initiator.TryAsOrRecursedFrom<CombatantCommandInitiator>(out var initiator))
+        {
+            return false;
+        }
+
+        return initiator.Initiator == id;
+    }
+
+    public string Describe(IDescriptionContext context)
+    {
+        return $"{context.InitiatorName} is {id}";
+    }
+}
---
+++ Whenever.Core/WheneverFilter/TargetIsExactly.cs
@@ -0,0 +1,23 @@
+﻿public class TargetIsExactly<TInspectWorld, TCommandWorld>: IWheneverFilter<TInspectWorld, TCommandWorld>
+    where TInspectWorld : IInspectWorld
+    where TCommandWorld : ICommandWorld
+{
+    private readonly CombatantId id;
+
+    public TargetIsExactly(CombatantId id)
+    {
+        this.id = id;
+    }
+    
+    public bool TriggersOn(InitiatedCommand<TCommandWorld> initiatedCommand, TInspectWorld world)
+    {
+        if (initiatedCommand.command is not IGenericTargetedWorldCommand<TCommandWorld> targetedCommand) return false;
+        
+        return targetedCommand.Target == id;
+    }
+
+    public string Describe(IDescriptionContext context)
+    {
+        return $"{context.TargetName} is {id}";
+    }
+}
