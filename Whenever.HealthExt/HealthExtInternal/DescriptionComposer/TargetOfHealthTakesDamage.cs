13a634227eb9b8b66a77ad7d6ade4a34a7746be5
Dan Miller <dan.miller@fraculation.com>
2024-02-25 13:13:25 -0600

    remove now unused implementations

--- Whenever.HealthExt/HealthExtInternal/DescriptionComposer/TargetOfHealthTakesDamage.cs
+++ Whenever.HealthExt/HealthExtInternal/DescriptionComposer/TargetOfHealthTakesDamage.cs
@@ -7,8 +7,6 @@ namespace HealthExtInternal.DescriptionComposer
     {
         public static IEnumerable<WheneverCompositeDescription<IInspectWorldHealth, ICommandWorldHealth>> CreateAll()
         {
-            yield return Create<DamageOccurs, TargetHasAtLeastHealth>((ctx, dmgOccurs, atLeastHealth) => 
-                    $"a {ctx.TargetName} with at least {atLeastHealth.atLeast} health takes {dmgOccurs.atLeast} damage");
             yield return Create<DamageOccurs, CombatantHasAtLeastHealth>((ctx, dmgOccurs, atLeastHealth) =>
                 $"{ctx.NameOf(atLeastHealth.combatant)} with at least {atLeastHealth.atLeast} health takes {dmgOccurs.atLeast} damage");
                 
